49869ac5f3f40e1294e75a992cc322b1a9a9cbfd
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-06-18 17:58:47 +0200

    documentation: wip

---
+++ docs/.vuepress/nav/en.js
@@ -0,0 +1,83 @@
+module.exports = [
+  {
+    text: 'Getting started',
+    link: '/guide/install/',
+  },
+  {
+    text: 'Foundation',
+    link: '/foundation/architecture',
+  },
+  {
+    text: 'Documentation',
+    link: '/documentation/service_provider',
+  },
+  {
+    text: 'About',
+    link: '/about/about',
+  },
+  
+  // {
+  //   text: 'Config Reference',
+  //   link: '/config/'
+  // },
+  // {
+  //   text: 'Plugin',
+  //   link: '/plugin/'
+  // },
+  // {
+  //   text: 'Theme',
+  //   link: '/theme/'
+  // },
+  // {
+  //   text: 'Learn More',
+  //   items: [
+  //     {
+  //       text: 'API',
+  //       items: [
+  //         {
+  //           text: 'CLI',
+  //           link: '/api/cli.html'
+  //         },
+  //         {
+  //           text: 'Node',
+  //           link: '/api/node.html'
+  //         },
+  //       ]
+  //     },
+  //     {
+  //       text: 'Contributing Guide',
+  //       items: [
+  //         {
+  //           text: 'Design Concepts',
+  //           link: '/miscellaneous/design-concepts.html'
+  //         },
+  //         {
+  //           text: 'FAQ',
+  //           link: '/faq/',
+  //         },
+  //         {
+  //           text: 'Glossary',
+  //           link: '/miscellaneous/glossary.html'
+  //         },
+  //       ]
+  //     },
+  //     {
+  //       text: 'Miscellaneous',
+  //       items: [
+  //         {
+  //           text: 'Migrate from 0.x',
+  //           link: '/miscellaneous/migration-guide.html'
+  //         },
+  //         {
+  //           text: 'Changelog',
+  //           link: 'https://github.com/vuejs/vuepress/blob/master/CHANGELOG.md'
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // },
+  // {
+  //   text: '0.x',
+  //   link: 'https://vuepress.vuejs.org/'
+  // },
+]
---
+++ docs/README.md
@@ -0,0 +1,19 @@
+---
+title: Ilos
+lang: en-US
+home: true
+heroText: Ilos
+tagline: Tralala
+actionText: Get Started →
+actionLink: /guide/
+features:
+  - title: Modulaire
+    details: Lorem ipsum
+  - title: Perfomant
+    details: Lorem ipsum
+  - title: Featured
+    details: Lorem ipsum
+footer: Apache 2.0 Licensed
+---
+
+# Hello
