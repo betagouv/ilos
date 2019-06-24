3691af72078c2ec20c40025c8bfa7f14cce693c6
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-06-24 11:14:43 +0200

    arch(cm): move connection manager

---
+++ packages/connection-manager/src/index.ts
@@ -0,0 +1,7 @@
+export { ConnectionManager } from './ConnectionManager';
+export {
+  ConnectionInterface,
+  ConnectionConfigType,
+  ConnectionDefinition,
+  ConnectionDeclaration
+} from './ConnectionManagerInterfaces';
