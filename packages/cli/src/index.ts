6442d163941a416753aa8538e093107a610818df
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-07-15 15:49:28 +0200

    arch: move bootstrap to framework

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,11 +3,8 @@ import { Command } from './parents';
 import { CommandRegistry } from './providers';
 import { CommandExtension } from './extensions/CommandExtension';
 import { CliTransport } from './transports/CliTransport';
-import { Bootstrap, bootstrap } from './Bootstrap';
 
 export {
-  Bootstrap,
-  bootstrap,
   CliTransport,
   CommandRegistry,
   Command,
